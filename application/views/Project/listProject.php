cb1abd45cbe569263db3ab20618e77d959255111
Shahzaad Ismail <shez@live.co.za>
2017-02-15 00:15:00 +0200

    added listing of project fields

--- application/views/Project/listProject.php
+++ application/views/Project/listProject.php
@@ -7,7 +7,16 @@ foreach ($collection as $item){
 	echo '
 	<table class="table table-striped">
 		<tr>
+			<td><b>Title:</b></td>
 			<td>'.$item->title.'</td>
+			
+
+		</tr>
+		
+		<tr>
+			<td><b>Description:</b></td>
+			<td>'.$item->description.'</td>
+
 		</tr>
 	</table>	
 	';
