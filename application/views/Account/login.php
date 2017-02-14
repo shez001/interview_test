30a292afe3bc019e2283aa8c83d55e0f3ee713b1
Shahzaad Ismail <shez@live.co.za>
2017-02-14 04:19:32 +0200

    updated login form

--- application/views/Account/login.php
+++ application/views/Account/login.php
@@ -1,4 +1,20 @@
-<form>
-<input type="text" name="username" />
-<input type="password" name="password" />
+ <form class="form-horizontal">
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="email">Email:</label>
+    <div class="col-sm-2">
+      <input type="email" class="form-control" id="email" placeholder="Enter email">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="password">Password:</label>
+    <div class="col-sm-2">
+      <input type="password" class="form-control" id="password" placeholder="Enter password">
+    </div>
+  </div>
+ 
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-default">Login</button>
+    </div>
+  </div>
 </form>
