6d6c4fafde0cade9751d2d6204f61bdd4e01186e
Shahzaad Ismail <shez@live.co.za>
2017-02-15 00:25:50 +0200

    model added and config encryption key added

---
+++ application/models/user.php
@@ -0,0 +1,30 @@
+<?php
+Class User extends CI_Model
+{
+
+ /**
+ @abstract model to cotrol login function
+ @param username - takes username
+ @param password - takes password
+ */
+ function login($username, $password)
+ {
+   $this -> db -> select('id, username, password');
+   $this -> db -> from('users');
+   $this -> db -> where('username', $username);
+   $this -> db -> where('password', MD5($password));
+   $this -> db -> limit(1);
+ 
+   $query = $this -> db -> get();
+ 
+   if($query -> num_rows() == 1)
+   {
+     return $query->result();
+   }
+   else
+   {
+     return false;
+   }
+ }
+}
+?>
