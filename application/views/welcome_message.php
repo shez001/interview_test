6e38b9c4ac9949a462e71490ce74b2be617fb08d
Shahzaad Ismail <shez@live.co.za>
2017-02-17 08:49:08 +0200

    improved layout style

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
+	 <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Welcome</title>
 <?php
 	/**
@@ -26,16 +27,88 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	';
 
 ?>
+
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    
+    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
+    .row.content {height: 450px}
+    
+    /* Set gray background color and 100% height */
+    .sidenav {
+      padding-top: 20px;
+      background-color: #f1f1f1;
+      height: 100%;
+    }
+    
+    /* Set black background color, white text and some padding */
+    footer {
+      background-color: #555;
+      color: white;
+      padding: 15px;
+    }
+    
+    /* On small screens, set height to 'auto' for sidenav and grid */
+    @media screen and (max-width: 767px) {
+      .sidenav {
+        height: auto;
+        padding: 15px;
+      }
+      .row.content {height:auto;} 
+    }
+  </style>
 	
 </head>
 <body>
 
-<div id="container">
-	<h1>Welcome</h1>
-	<?php 
-		echo $login;
-	
-	?>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="<?php echo base_url(); ?>">Home</a></li>
+        <li><a href="<?php echo site_url('project/listProject'); ?>">List Projects</a></li>
+      
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+       
+      </ul>
+    </div>
+  </div>
+</nav>
+  
+<div class="container-fluid text-center">    
+  <div class="row content">
+    <div class="col-sm-2 sidenav">
+    
+    </div>
+    <div class="col-sm-8 text-left"> 
+      <h1>Welcome</h1>
+      <hr>
+	  <?php 
+		echo $content;
+	  
+	  ?>
+    </div>
+    <div class="col-sm-2 sidenav">
+    </div>
+  </div>
 </div>
+
+<footer class="container-fluid text-center">
+  
+</footer>
+
 </body>
 </html>
